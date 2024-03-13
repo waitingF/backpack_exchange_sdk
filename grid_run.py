865d1e83cca99bae60484e14d879a18361e6b57e
kongwei <kongwei@pku.edu.cn>
2024-03-13 08:43:02 +0800

    cache

---
+++ grid_run.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+from grid_data import RunBetData
+import time
+
+from backpack_exchange_sdk.authenticated import AuthenticationClient
+from backpack_exchange_sdk.public import PublicClient
+from config import key, secret
+
+runbet = RunBetData()
+
+
+SOL_USDC = "SOL_USDC"
+client = AuthenticationClient()
+client.setup(key, secret)
+
+def get_price():
+    tick = PublicClient().get_ticker(SOL_USDC)
+    return float(tick['lastPrice'])
+    # print(PublicClient().get_markets())
+
+
+
+class Run_Main():
+
+    def __init__(self):
+        self.coinType = runbet.get_cointype()  # 交易币种
+        pass
+
+    def loop_run(self):
+        while True:
+            cur_market_price = get_price() # 当前交易对市价
+            grid_buy_price = runbet.get_buy_price()  # 当前网格买入价格
+            grid_sell_price = runbet.get_sell_price() # 当前网格卖出价格
+            quantity = runbet.get_quantity()   # 买入量
+            step = runbet.get_step() # 当前步数
+
+            if grid_buy_price >= cur_market_price:   # 是否满足买入价
+                # res = msg.buy_limit_msg(self.coinType, quantity, grid_buy_price)
+                print("以价格 %s 买入 %s" % (grid_buy_price, quantity))
+                res = client.execute_order("Limit", "Bid", SOL_USDC, quantity=quantity, price=str(grid_buy_price))
+                if res['id']: # 挂单成功
+                    runbet.modify_price(grid_buy_price, step+1) #修改data.json中价格、当前步数
+                    time.sleep(60*2) # 挂单后，停止运行1分钟
+                else:
+                    break
+
+            elif grid_sell_price < cur_market_price:  # 是否满足卖出价
+                if step==0: # setp=0 防止踏空，跟随价格上涨
+                    runbet.modify_price(grid_sell_price,step)
+                else:
+                    # res = msg.sell_limit_msg(self.coinType, runbet.get_quantity(False), grid_sell_price)
+                    print("以价格 %s 卖出 %s" % (grid_sell_price, quantity))
+                    res = client.execute_order("Limit", "Ask", SOL_USDC, quantity=quantity, price=str(grid_sell_price))
+                    if res['id']:
+                        runbet.modify_price(grid_sell_price, step - 1)
+                        time.sleep(60*2)  # 挂单后，停止运行1分钟
+                    else:
+                        break
+            else:
+                print("当前市价：{market_price}。未能满足交易,继续运行".format(market_price = cur_market_price))
+                time.sleep(5)
+
+
+if __name__ == "__main__":
+    # print(get_price())
+    # print(runbet.get_buy_price())
+    # print(runbet.get_sell_price())
+    # exit()
+
+    instance = Run_Main()
+    try:
+        instance.loop_run()
+    except Exception as e:
+        error_info = "报警：币种 {coin},服务停止.错误原因 {info}".format(coin=instance.coinType,info=str(e))
+        print(error_info)
+
+# 调试看报错运行下面，正式运行用上面
+# if __name__ == "__main__":
+    # instance = Run_Main()
+    # instance.loop_run()
--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-import datetime
 import queue
 import threading
 import time
+from datetime import datetime
+
+import numpy as np
+import bisect
 
 from backpack_exchange_sdk.authenticated import AuthenticationClient
 from backpack_exchange_sdk.public import PublicClient
 from config import key, secret
-
 
 SOL_USDC = "SOL_USDC"
 
@@ -27,27 +29,76 @@ class TickerThread(threading.Thread):
         while not self.shut_down:
             tick = self.public_client.get_ticker(SOL_USDC)
             tick_queue.put(tick)
-            time.sleep(1)
+            time.sleep(2)
 
     def close(self):
         self.shut_down = True
 
-
+    # init
+    # 2709.89 USDC
 if __name__ == "__main__":
-    #
-    # # Get all supported assets
-    # assets = public_client.get_assets()
-    # print(assets)
-    #
-    # # Get ticker information for a specific symbol
+    _low = 120
+    _high = 160
+    grids = [i * 1.0 for i in range(_low, _high + 1, 1)]
+    holds = [0.0 for i in range(_low, _high + 1, 1)]
+    print(grids, len(grids))
+
+    last_grid = 0
+
+    def find_grid(price: float) -> int:
+        res = 0
+        for i in range(len(grids)):
+            if grids[i] > price:
+                break
+            else:
+                res = i
+        return res
+
+    # print(110, find_grid(110), grids[find_grid(110)])
+    # print(120, find_grid(120), grids[find_grid(120)])
+    # print(121, find_grid(121), grids[find_grid(121)])
+    # print(121.1, find_grid(121.1), grids[find_grid(121.1)])
+    # print(128.1, find_grid(128.1), grids[find_grid(128.1)])
+    # print(160, find_grid(160), grids[find_grid(160)])
+    # print(170, find_grid(170), grids[find_grid(170)])
 
     tickers = TickerThread()
     tickers.start()
 
 
 
-    client = AuthenticationClient()
-    client.setup(key, secret)
+    tick_count = 0
+    while True:
+        tick_count += 1
+        tick = tick_queue.get()
+        last_price = tick['lastPrice']
+        grid_id = find_grid(float(last_price))
+        # print(datetime.now(), grid_id, grids[grid_id], last_price)
+        quantity = 0.2
+
+        if tick_count % 30 == 0:
+            print(datetime.now(), grid_id, grids[grid_id], last_price)
+
+        if last_grid == 0:
+            last_grid = grid_id
+            print("初始价格 %s 网格id=%s" % (last_price, grid_id))
+            continue
+
+        stride = grid_id - last_grid
+        if stride == 0: # 同一个网格，不卖出
+            pass
+        elif stride > 0: # 网格往上走了，卖出
+
+            print("以价格 %s 卖出 %s" % (last_price, quantity * stride))
+            pass
+        else: # 网格往下走了，买入
+
+            print("以价格 %s 买入 %s" % (last_price, quantity * (-1 * stride)))
+            # client.execute_order("Limit", "Bid", SOL_USDC, quantity=quantity, price=str(last_price))
+            pass
+
+        last_grid = grid_id
+
 
 
     # Get account balances
@@ -59,26 +110,33 @@ if __name__ == "__main__":
     print(client.get_open_orders(SOL_USDC))
     # print(client.execute_order("Limit", "Bid", SOL_USDC, quantity="0.01", price="152.2"))
 
-
-    # Request a withdrawal
-    # response = client.request_withdrawal('xxxxaddress', 'Solana', '0,1', 'Sol')
-    # print(response)
-
-    # public_client = PublicClient()
-    #
-    # # Get all supported assets
-    # assets = public_client.get_assets()
-    # print(assets)
-    #
-    # # Get ticker information for a specific symbol
-    # ticker = public_client.get_ticker('SOL_USDC')
-    # print(ticker)
-
-    # print(PublicClient().get_klines(SOL_USDC, "1m"))
-
-
     # while True:
     #     tick = tick_queue.get()
     #     print(datetime.datetime.now(), tick)
 
     tickers.close()
+
+
+    '''
+buy
+{
+  "symbol": "SOL_USDC",
+  "side": "Bid",
+  "orderType": "Limit",
+  "quantity": "1.00",
+  "price": "149.50",
+  "postOnly": false
+}
+
+sell
+{
+  "symbol": "SOL_USDC",
+  "side": "Ask",
+  "orderType": "Limit",
+  "quantity": "0.01",
+  "price": "150.50",
+  "postOnly": false
+}
+    
+    
+    '''
